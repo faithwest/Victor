79c0b6e002bd1cd08255c6a189b7bb0fa3354f65
faithwest <faithchepkoech425@gmail.com>
2023-12-08 00:12:26 +0300

    creaated,wrote code for mycontext .js component

---
+++ VICTOR/JS/ MyContext.js
@@ -0,0 +1,14 @@
+// MyContext.js
+import React, { createContext, useState } from 'react';
+
+export const MyContext = createContext();
+
+export const MyContextProvider = ({ children }) => {
+  const [websiteName, setWebsiteName] = useState('Videography Website');
+
+  return (
+    <MyContext.Provider value={{ websiteName, setWebsiteName }}>
+      {children}
+    </MyContext.Provider>
+  );
+};
