f104df1a47a1976a87cd4c20870dd1be8d8245bb
faithwest <faithchepkoech425@gmail.com>
2023-12-13 23:02:28 +0300

    added a new portfolio page component

---
+++ VICTOR/REACT/portfolio.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PortfolioPage = () => {
+  return (
+    <div>
+      <h2>Portfolio</h2>
+      {/* Display a grid or carousel of videography work */}
+      {/* Include project titles, descriptions, and videos */}
+    </div>
+  );
+};
+
+export default PortfolioPage;
