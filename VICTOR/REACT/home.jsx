a09a9969b027208f8deb1197429796b275cf1dee
faithwest <faithchepkoech425@gmail.com>
2023-12-13 22:40:40 +0300

    Update home component

--- VICTOR/REACT/home.jsx
+++ VICTOR/REACT/home.jsx
@@ -1,1 +1,18 @@
-I
+I// Home.js
+
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to My Home Page</h1>
+      <p>This is a simple React home page.</p>
+      <img
+        src="https://placekitten.com/800/400" // Placeholder image (replace with your own image)
+        alt="Cute Kitten"
+      />
+    </div>
+  );
+};
+
+export default Home;
